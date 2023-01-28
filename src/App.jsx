0d1bd614605822b49084e1d9ebf0088dba0742f1
thisissamridh <samridhsinghind@gmail.com>
2023-01-29 00:01:19 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createRoot  } from "react-dom";
+
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Havanese"),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
